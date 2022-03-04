20823a268084816f32e0134836316795fbdf422e
Trevor Tanner <trevtanner@gmail.com>
2022-03-03 19:18:56 -0800

    Connected to PokeAPI

--- src/components/DexListItem.js
+++ src/components/DexListItem.js
@@ -2,13 +2,18 @@ import { ListItem, Avatar, ListItemButton, ListItemText, Divider } from '@mui/ma
 import { Fragment } from 'react';
 
 function DexListItem(props) {
+
+  function capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  
     return (
     <Fragment>
         <ListItem>
         <ListItemButton>
         <Avatar src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' sx= {{ paddingRight: '20px', width: 56, height: 56 }} />
-        <ListItemText primary={props.name} secondary={props.id} sx={{ textAlign: 'start' }} />
-        <ListItemText secondary={`${props.firstType}/${props.secondType}`} sx={{ textAlign: 'end' }} />
+        <ListItemText primary={props.name.charAt(0).toUpperCase() + props.name.slice(1)} /*secondary={props.id}*/ sx={{ textAlign: 'start' }} />
+        {/* <ListItemText secondary={`${props.firstType}/${props.secondType}`} sx={{ textAlign: 'end' }} /> */}
         </ListItemButton>
       </ListItem>
       <Divider />
--- src/components/PokedexList.js
+++ src/components/PokedexList.js
@@ -1,155 +1,162 @@
-import { Box, List, Divider } from "@mui/material";
-import { useEffect, useState } from "react";
-import DexListItem from "./DexListItem";
-import axios from 'axios';
+import { Box, List, Divider } from '@mui/material'
+import { useEffect, useState } from 'react'
+import DexListItem from './DexListItem'
+import axios from 'axios'
 
 function PokedexList() {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     number: "001",
-  //     name: "Bulbasaur",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 2,
-  //     number: "002",
-  //     name: "Ivysaur",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 3,
-  //     number: "003",
-  //     name: "Venusaur",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 4,
-  //     number: "004",
-  //     name: "Charmander",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 5,
-  //     number: "005",
-  //     name: "Charmeleon",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 6,
-  //     number: "006",
-  //     name: "Charizard",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 7,
-  //     number: "007",
-  //     name: "Squirtle",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 8,
-  //     number: "008",
-  //     name: "Wartortle",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 9,
-  //     number: "009",
-  //     name: "Blastoise",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 10,
-  //     number: "010",
-  //     name: "Caterpie",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 11,
-  //     number: "011",
-  //     name: "Metapod",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 12,
-  //     number: "012",
-  //     name: "Butterfree",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 13,
-  //     number: "013",
-  //     name: "Weedle",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 14,
-  //     number: "014",
-  //     name: "Kakuna",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 15,
-  //     number: "015",
-  //     name: "Beedrill",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 16,
-  //     number: "016",
-  //     name: "Pidgey",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 17,
-  //     number: "017",
-  //     name: "Pigeotto",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 18,
-  //     number: "018",
-  //     name: "Pidgeot",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 19,
-  //     number: "019",
-  //     name: "Rattata",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  //   {
-  //     id: 20,
-  //     number: "020",
-  //     name: "Raticate",
-  //     types: [{ first: "Grass", second: "poison" }],
-  //   },
-  // ];
+  const data = [
+    {
+      id: 1,
+      number: '001',
+      name: 'Bulbasaur',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 2,
+      number: '002',
+      name: 'Ivysaur',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 3,
+      number: '003',
+      name: 'Venusaur',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 4,
+      number: '004',
+      name: 'Charmander',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 5,
+      number: '005',
+      name: 'Charmeleon',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 6,
+      number: '006',
+      name: 'Charizard',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 7,
+      number: '007',
+      name: 'Squirtle',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 8,
+      number: '008',
+      name: 'Wartortle',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 9,
+      number: '009',
+      name: 'Blastoise',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 10,
+      number: '010',
+      name: 'Caterpie',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 11,
+      number: '011',
+      name: 'Metapod',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 12,
+      number: '012',
+      name: 'Butterfree',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 13,
+      number: '013',
+      name: 'Weedle',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 14,
+      number: '014',
+      name: 'Kakuna',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 15,
+      number: '015',
+      name: 'Beedrill',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 16,
+      number: '016',
+      name: 'Pidgey',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 17,
+      number: '017',
+      name: 'Pigeotto',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 18,
+      number: '018',
+      name: 'Pidgeot',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 19,
+      number: '019',
+      name: 'Rattata',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+    {
+      id: 20,
+      number: '020',
+      name: 'Raticate',
+      types: [{ first: 'Grass', second: 'poison' }],
+    },
+  ]
 
-  const [loading, setLoading] = useState(false);
-  const [entries, setEntries] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [entries, setEntries] = useState([])
 
   useEffect(() => {
-    const loadPokemon = async () => {
-      setLoading(true)
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=151/',
+      )
+      const data = await response.json()
 
-      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151/");
-
-      setEntries(response.data)
-
+      setEntries(data.results)
       setLoading(false)
     }
-    loadPokemon();
-  }, []);
+    fetchData()
+  }, [])
 
+  if (loading) {
+    return <p>Loading Entries</p>
+  }
 
+  const pokemonList = entries.map((entry) => (
+    <DexListItem key={entry.url} name={entry.name} />
+  ))
   return (
-    <Box sx={{ width: "100%", maxWidth: "500px", margin: "auto" }}>
+    <Box sx={{ width: '100%', maxWidth: '700px', margin: 'auto' }}>
       <nav aria-label="">
         <List>
-          {entries.map((item) => (
+          {/* {data.map((item) => (
             <DexListItem
+              key={item.number}
               id={`# ${item.number}`}
               name={item.name}
               firstType={item.types.map((type) => (
@@ -159,11 +166,12 @@ function PokedexList() {
                 type.second
               ))}
             />
-          ))}
+          ))} */}
+          {pokemonList}
         </List>
       </nav>
     </Box>
-  );
+  )
 }
 
-export default PokedexList;
+export default PokedexList
