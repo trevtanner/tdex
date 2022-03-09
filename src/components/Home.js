bcde88cf12e43d36f423a5c8543927a487a3911c
Trevor Tanner <trevor.tanner.developer@gmail.com>
2022-03-08 19:09:36 -0800

    readme changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,33 @@
-import { Container, Grid, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
-import { useState } from 'react';
-import PokedexList from './PokedexList';
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { useState } from "react";
+import PokedexList from "./PokedexList";
 
 function Home() {
-
-const [genSelect, setGenSelect] = useState('')
+  const [genSelect, setGenSelect] = useState("");
 
   function gen1SelectHandler(event) {
     event.preventDefault();
-    setGenSelect('gen1')
-
-
+    setGenSelect("gen1");
   }
 
-    return (
-        <Container sx={{ textAlign: 'center' }}>
-     <h1>PokeDex</h1>
-     <Grid container>
-     <Grid item xs={4} lg={3} sx={{ width: '100%' }}>
-     <List>
-      <ListItem>
-      <ListItemButton>
-        <ListItemText primary='Gen 1' onClick={gen1SelectHandler}/>
-        </ListItemButton>
-      </ListItem>
-      {/* <ListItem>
+  return (
+    <Container sx={{ textAlign: "center" }}>
+      <Grid container>
+        <Grid item xs={4} lg={3} sx={{ width: "100%" }}>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary="Gen 1" onClick={gen1SelectHandler} />
+              </ListItemButton>
+            </ListItem>
+            {/* <ListItem>
       <ListItemButton>
         <ListItemText primary='Gen 2' />
         </ListItemButton>
@@ -34,14 +37,14 @@ const [genSelect, setGenSelect] = useState('')
         <ListItemText primary='Gen 3' />
         </ListItemButton>
       </ListItem> */}
-     </List>
-     </Grid>
-     <Grid item xs={8} lg={9}>
-     <PokedexList className='PokeList'/>
-     </Grid>
-     </Grid>
-     </Container>
-    )
+          </List>
+        </Grid>
+        <Grid item xs={8} lg={9}>
+          <PokedexList className="PokeList" />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Home;
